03830d0c400cb979d5b823420403537cdf613d7a
Tanner Nelson <tanner@bluebite.com>
2016-02-21 17:24:48 -0500

    naming

--- Package.swift
+++ Package.swift
@@ -3,8 +3,7 @@ import PackageDescription
 let package = Package(
     name: "FluentSQLite",
     dependencies: [ 
-   		.Package(url: "https://github.com/tannernelson/csqlite.git", majorVersion: 0),
-        //.Package(url: "https://github.com/kylef/Spectre.git", majorVersion: 0) //testing
-        .Package(url: "https://github.com/tannernelson/fluent.git", majorVersion: 0)
+   		.Package(url: "https://github.com/qutheory/csqlite.git", majorVersion: 0),
+        .Package(url: "https://github.com/qutheory/fluent.git", majorVersion: 0)
     ]
 )

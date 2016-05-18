1321d1a699a9d4ecbd622eadeb7ead99d16e0e8c
Tanner Nelson <tanner@bluebite.com>
2016-05-18 16:21:24 -0400

    add raw

--- Sources/SQLiteDriver.swift
+++ Sources/SQLiteDriver.swift
@@ -28,27 +28,9 @@ public class SQLiteDriver: Fluent.Driver {
     public func execute<T: Model>(_ query: Query<T>) throws -> [[String: Value]] {
         let sql = SQL(query: query)
         
-        var results: [SQLite.Result.Row]
-
         print("SQLite executing: \(sql.statement)")
-        results = try database.execute(sql.statement) { preparer in
-            for value in sql.values {
-                switch value.structuredData {
-                case .integer(let int):
-                    try preparer.bind(int)
-                case .double(let double):
-                    try preparer.bind(double)
-                case .string(let string):
-                    try preparer.bind(string)
-                case .array(_):
-                    throw Error.unsupported("Array values not supported.")
-                case .dictionary(_):
-                    throw Error.unsupported("Dictionary values not supported.")
-                case .null: break
-                case .bool(let bool):
-                    try preparer.bind(bool)
-                }
-            }
+        let results = try database.execute(sql.statement) { preparer in
+            try self.bind(preparer: preparer, to: sql.values)
         }
 
         if query.action == .create {
@@ -56,13 +38,49 @@ public class SQLiteDriver: Fluent.Driver {
                [idKey : database.lastId]
             ]
         } else {
-            return results.map { row in
-                var data: [String: Value] = [:]
-                row.data.forEach { key, val in
-                    data[key] = val
-                }
-                return data
+            return map(results: results)
+        }
+    }
+
+    /**
+        Executes a raw query with an 
+        optional array of paramterized
+        values and returns the results.
+    */
+    public func raw(_ statement: String, values: [Value] = []) throws -> [[String: Value]] {
+        let results = try database.execute(statement) { preparer in
+            try self.bind(preparer: preparer, to: values)
+        }
+        return map(results: results)
+    }
+
+    func bind(preparer: SQLite, to values: [Value]) throws {
+        for value in values {
+            switch value.structuredData {
+            case .integer(let int):
+                try preparer.bind(int)
+            case .double(let double):
+                try preparer.bind(double)
+            case .string(let string):
+                try preparer.bind(string)
+            case .array(_):
+                throw Error.unsupported("Array values not supported.")
+            case .dictionary(_):
+                throw Error.unsupported("Dictionary values not supported.")
+            case .null: break
+            case .bool(let bool):
+                try preparer.bind(bool)
             }
+        }
+    }
+
+    func map(results: [SQLite.Result.Row]) -> [[String: Value]] {
+        return results.map { row in
+            var data: [String: Value] = [:]
+            row.data.forEach { key, val in
+                data[key] = val
+            }
+            return data
         }
     }
 

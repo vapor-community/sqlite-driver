abf3596bb71d0ad1039a9468f80e3529b828795c
Tanner Nelson <tanner@bluebite.com>
2016-06-08 15:03:21 -0400

    test updates

--- Tests/FluentSQLite/SQLite3Tests.swift
+++ Tests/FluentSQLite/SQLite3Tests.swift
@@ -4,42 +4,7 @@ import Fluent
 @testable import FluentSQLite
 
 class SQLite3Tests: XCTestCase {
-    static var allTests : [(String, SQLite3Tests -> () throws -> Void)] {
-        return [
-            ("testBasicSelect", testBasicSelect),
-        ]
-    }
-
-    final class User: Model {
-        var id: Value?
-
-        func serialize() -> [String: Value?] {
-            return [:]
-        }
-
-        init?(serialized: [String: Value]) {
-
-        }
-    }
-
-    var driver: SQLiteDriver!
-    var database: Database!
-
-    override func setUp() {
-
-
-        driver = try! SQLiteDriver(path: "Tests/test.sqlite")
-        database = Database(driver: driver)
-        Database.default = database
-    }
-
-    func testBasicSelect() {
-        let token = "500D4770-9720-44DF-A25E-F29689DA521D"
-        do {
-            let user = try User.query.filter("token", token).first()
-            XCTAssert(user?.id?.string == "1", "Correct User not found")
-        } catch {
-            XCTFail("Could not query: \(error)")
-        }
+    func testReality() {
+        XCTAssert(2 + 2 == 4, "Something is seriously wrong.")
     }
 }

60131297867b75719ea329a0a973091ed5ecb72c
Tanner Nelson <tanner@bluebite.com>
2016-06-08 16:44:15 -0400

    fluent 5 (0 for now)

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "FluentSQLite",
     dependencies: [ 
         .Package(url: "https://github.com/qutheory/csqlite.git", majorVersion: 0, minor: 1),
-        .Package(url: "https://github.com/qutheory/fluent.git", majorVersion: 0, minor: 3)
+        .Package(url: "https://github.com/qutheory/fluent.git", majorVersion: 0, minor: 0)
     ]
 )

df59a47ea7a129b3d7631f1798280cc1fd80071c
Adrien Cantérot <adrien.canterot@gmail.com>
2016-09-03 17:05:21 +0200

    forked version of sqlite

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "FluentSQLite",
     dependencies: [
-        .Package(url: "https://github.com/vapor/sqlite.git", majorVersion: 0),
+        .Package(url: "https://github.com/adriencanterot/sqlite.git", majorVersion: 0),
         .Package(url: "https://github.com/vapor/fluent.git", majorVersion: 0, minor: 10),
     ]
 )

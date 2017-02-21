3c39279fcace6351274fd05fc208f08f967c84e1
Logan Wright <logan.william.wright@gmail.com>
2017-02-21 17:20:28 +0100

    update dependencies

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "FluentSQLite",
     dependencies: [
-        .Package(url: "https://github.com/vapor/sqlite.git", Version(0,0,0)),
+        .Package(url: "https://github.com/vapor/sqlite.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"])),
         .Package(url: "https://github.com/vapor/fluent.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"])),
     ]
 )
